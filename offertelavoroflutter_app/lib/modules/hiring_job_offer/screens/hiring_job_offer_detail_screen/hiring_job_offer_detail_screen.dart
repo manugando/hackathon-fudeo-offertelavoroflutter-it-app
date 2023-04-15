3715ecb6ab95e030818d215dd270815e7c38c59e
Emanuele Gandolfi <emanuele.gandolfi89@gmail.com>
2023-04-16 01:10:22 +0200

    Add hiring job offer detail screen bloc

--- offertelavoroflutter_app/lib/modules/hiring_job_offer/screens/hiring_job_offer_detail_screen/hiring_job_offer_detail_screen.dart
+++ offertelavoroflutter_app/lib/modules/hiring_job_offer/screens/hiring_job_offer_detail_screen/hiring_job_offer_detail_screen.dart
@@ -1,47 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:offertelavoroflutter_app/helpers/app_share.dart';
 import 'package:offertelavoroflutter_app/modules/app_shell/widgets/detail_app_bar.dart';
 import 'package:offertelavoroflutter_app/modules/common/widgets/multi_style_text.dart';
 import 'package:offertelavoroflutter_app/modules/hiring_job_offer/models/hiring_job_offer/hiring_job_offer.dart';
+import 'package:offertelavoroflutter_app/modules/hiring_job_offer/repositories/hiring_job_offer_repository.dart';
+import 'package:offertelavoroflutter_app/modules/hiring_job_offer/screens/hiring_job_offer_detail_screen/bloc/hiring_job_offer_detail_screen_bloc.dart';
 import 'package:offertelavoroflutter_app/modules/hiring_job_offer/widgets/hiring_job_offer_detail_info.dart';
 import 'package:offertelavoroflutter_app/modules/hiring_job_offer/widgets/hiring_job_offer_detail_title.dart';
 
 class HiringJobOfferDetailScreen extends StatelessWidget {
-  final ScrollController scrollController = ScrollController();
   final HiringJobOffer hiringJobOffer;
-
-  HiringJobOfferDetailScreen({Key? key, required this.hiringJobOffer}) : super(key: key);
+  const HiringJobOfferDetailScreen({Key? key, required this.hiringJobOffer}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: hiringJobOffer.id,
-      child: Scaffold(
-        appBar: DetailAppBar(
-          actions: _buildActions(context),
-          title: _buildAppBarTitle(context),
-          scrollController: scrollController,
-        ),
-        body: SingleChildScrollView(
-          controller: scrollController,
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                HiringJobOfferDetailTitle(hiringJobOffer: hiringJobOffer),
-                HiringJobofferDetailInfo(hiringJobOffer: hiringJobOffer),
-                const SizedBox(height: 20),
-              ],
-            )
-          ),
-        ),
+      child: BlocProvider(
+        create: (context) => HiringJobOfferDetailScreenBloc(
+          hiringJobOfferRepository: RepositoryProvider.of<HiringJobOfferRepository>(context),
+          hiringJobOffer: hiringJobOffer
+        )..add(const HiringJobOfferDetailScreenEvent.initialized()),
+        child: _HiringJobOfferDetailView(),
       ),
     );
   }
+}
 
-  Widget _buildAppBarTitle(BuildContext context) {
+class _HiringJobOfferDetailView extends StatelessWidget {
+  final ScrollController scrollController = ScrollController();
+
+  _HiringJobOfferDetailView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HiringJobOfferDetailScreenBloc, HiringJobOfferDetailScreenState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: DetailAppBar(
+            actions: _buildActions(state.hiringJobOffer, state.isFavorite, context),
+            title: _buildAppBarTitle(state.hiringJobOffer, context),
+            scrollController: scrollController,
+          ),
+          body: SingleChildScrollView(
+            controller: scrollController,
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  HiringJobOfferDetailTitle(hiringJobOffer: state.hiringJobOffer),
+                  HiringJobofferDetailInfo(hiringJobOffer: state.hiringJobOffer),
+                  const SizedBox(height: 20),
+                ],
+              )
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildAppBarTitle(HiringJobOffer hiringJobOffer, BuildContext context) {
     if(hiringJobOffer.name == null || hiringJobOffer.name!.isEmpty) return const SizedBox();
 
     return MultiStyleText(
@@ -53,13 +75,21 @@ class HiringJobOfferDetailScreen extends StatelessWidget {
     );
   }
 
-  List<Widget> _buildActions(BuildContext context) {
+  List<Widget> _buildActions(HiringJobOffer hiringJobOffer, bool isFavorite, BuildContext context) {
     return [
       IconButton(
         onPressed: () {
-          // TODO
+          context.read<HiringJobOfferDetailScreenBloc>()
+            .add(const HiringJobOfferDetailScreenEvent.favoriteHiringJobOfferToggled());
+
+          SnackBar snackBar = SnackBar(
+            content: Text(isFavorite ? AppLocalizations.of(context)!.jobOfferRemovedFromFavorites : AppLocalizations.of(context)!.jobOfferAddedToFavorites,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.onPrimary)
+            )
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
         },
-        icon: SvgPicture.asset('assets/icons/bookmark.svg')
+        icon: SvgPicture.asset(isFavorite ? 'assets/icons/bookmark-filled.svg' : 'assets/icons/bookmark.svg')
       ),
       IconButton(
         onPressed: () => AppShare.share(AppLocalizations.of(context)!.jobOfferShareText(hiringJobOffer.url)),
