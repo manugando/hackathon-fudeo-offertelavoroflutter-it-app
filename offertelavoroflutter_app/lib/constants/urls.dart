ab1bc1df58ff9602a5af756c37355380569aa10c
Emanuele Gandolfi <emanuele.gandolfi89@gmail.com>
2023-04-19 22:43:02 +0200

    Add publish job offer screen content

--- offertelavoroflutter_app/lib/constants/urls.dart
+++ offertelavoroflutter_app/lib/constants/urls.dart
@@ -1,5 +1,9 @@
 class Urls {
   static const String hiringJobOfferNewsletterForm = 'https://forms.gle/nsZASph9FhUfQBAo9';
+  static const String publishHiringJobOfferForm = 'https://forms.gle/oJkCgbUvyYmkVpe79';
+
   static const String freelanceJobOfferNewsletterForm = 'https://forms.gle/Zv88pAV4H9MAPTb36';
+  static const String publishFreelanceJobOfferForm = 'https://forms.gle/5PEWCuZAD5eHzHYt5';
+
   static const String ecosistemaFlutterItalia = 'https://offertelavoroflutter.it/8b04bb0e6b2342209b5843aca9b3424f';
 }
